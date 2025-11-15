17951680216bd49206aa1ca4b92099fd8908339c
lmaos <54808358+lmaos-code@users.noreply.github.com>
2025-11-15 14:48:20 +0100

    feat: pinned posts (#4)
    * feat: pinned posts

--- src/lib/garden/preview.svelte
+++ src/lib/garden/preview.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import fwd_arrow from '$lib/assets/svg/forward_arrow.svg';
+	import pin from '$lib/assets/svg/pin.svg';
 	export let title: string;
 	export let slug: string;
 	export let excerpt: string;
 	export let date: string;
+	export let pinned: boolean;
 
-	// Format the date nicely
-	const formattedDate = new Date(date).toLocaleDateString('en-US', {
+	const formattedDate = new Date(date).toLocaleDateString('en-UK', {
 		year: 'numeric',
 		month: 'long',
 		day: 'numeric'
@@ -16,7 +17,14 @@
 <a href="/garden/{slug}" class="block hover:no-underline h-full">
 	<article class="p-6 md:p-8 transition-all hover:bg-[#5f895d15] group h-full">
 		<div class="flex flex-col h-full">
-			<time class="text-sm opacity-70 mb-3">{formattedDate}</time>
+			<div class="flex items-center justify-between">
+				<time class="text-sm opacity-70">{formattedDate}</time>
+				{#if pinned}
+					<div class="flex items-center gap-2 text-accent text-sm">
+						<img src={pin} alt="Pin" class="size-5" />
+					</div>
+				{/if}
+			</div>
 			<h2
 				class="text-xl md:text-2xl font-bold group-hover:text-[var(--color-accent)] transition-colors mb-3"
 			>
